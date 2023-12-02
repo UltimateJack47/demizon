d766cb4bbe66a0ac19f7c795890a8a1f3b8e652c
Josef Obrtlik <obrtlik@mathesio.com>
2023-12-02 15:52:01 +0100

    Added missing public modifier

--- Demizon.Core/Services/IFileUploadService.cs
+++ Demizon.Core/Services/IFileUploadService.cs
@@ -2,5 +2,6 @@
 
 public interface IFileUploadService
 {
-    Task<FileUploadResult> UploadImageAsync(FileUploadRequest file, bool createResizedImages = true, string? uploadSessionIdentifier = null);
+    public Task<FileUploadResult> UploadImageAsync(FileUploadRequest file, bool createResizedImages = true,
+        string? uploadSessionIdentifier = null);
 }
